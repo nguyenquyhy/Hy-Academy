489a2149a3d457ef15ec85a103353027053a0168
Nguyễn Quý Hy <nguyenquyhy@live.com.sg>
2021-12-20 22:15:25 +0800

    Add client support for GraphQL (#9)

---
+++ React/apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    client: {
+        service: {
+            name: 'academy-graphql',
+            localSchemaFile: './React/schema.graphql'
+        },
+        excludes: ['src/types.ts']
+    }
+};
