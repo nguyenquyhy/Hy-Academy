e831e6ac154e3e01357b82fe22704c9dae0c95d9
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2021-12-25 00:48:47 +0800

    Add Data project

--- Api/HyAcademy.GraphQL/Query.cs
+++ Api/HyAcademy.GraphQL/Query.cs
@@ -1,6 +1,10 @@
-﻿namespace HyAcademy.GraphQL;
+﻿using HyAcademy.Data;
+
+namespace HyAcademy.GraphQL;
 
 public class Query
 {
     public int GetValue() => 1;
+
+    public IQueryable<Course> GetCourses() => new List<Course>().AsQueryable();
 }
