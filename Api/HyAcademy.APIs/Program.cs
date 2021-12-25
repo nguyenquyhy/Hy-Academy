095938585a9f934bacdcdb413415847757492b18
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2021-12-26 01:27:36 +0800

    Add HyAcademy.GraphQL.Tools to generate schema

--- Api/HyAcademy.APIs/Program.cs
+++ Api/HyAcademy.APIs/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthorization();
 builder.Services
     .AddGraphQLServer()
-    .AddQueryType<Query>();
+    .ConfigureGraphQL();
 
 var app = builder.Build();
 
