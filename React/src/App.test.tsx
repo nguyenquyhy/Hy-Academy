56d9436c53ec6bd217d7f0887c410b022dfaa97b
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2021-12-26 10:09:39 +0800

    Add sample tests for Apollo hooks

--- React/src/App.test.tsx
+++ React/src/App.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
 import App from './App';
 
 test('renders welcome', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome/i);
-  expect(linkElement).toBeInTheDocument();
+    render(
+        <MockedProvider>
+            <App />
+        </MockedProvider>
+    );
+    const linkElement = screen.getByText(/Welcome/i);
+    expect(linkElement).toBeInTheDocument();
 });
