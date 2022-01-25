51fe3b35743dcc93fa7a6a5330e25f6bd5bc18e1
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2022-01-25 13:12:18 -0500

    Add logic for collapsed navbar

--- React/src/layouts/TopMenu.tsx
+++ React/src/layouts/TopMenu.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import Icon from 'images/icon-95x56.png';
+import { useState } from 'react';
+import classNames from 'classnames';
 
 const menu = [
     {
@@ -28,63 +30,73 @@ const menu = [
     }
 ]
 
-const TopMenu = () => (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-            <Link className="navbar-item" to="/" title="Hy Academy">
-                <img src={Icon} height="28" alt="Hy Academy" />
-            </Link>
+const TopMenu = () => {
+    const [isMenuOpen, setMenuOpen] = useState(false);
 
-            <button className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </button>
-        </div>
+    return (
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <Link className="navbar-item" to="/" title="Hy Academy">
+                    <img src={Icon} height="28" alt="Hy Academy" />
+                </Link>
 
-        <div id="navbarBasicExample" className="navbar-menu">
-            <div className="navbar-start">
-                {menu.map(item => item.items ? 
-                    (
-                        <div key={item.text} className="navbar-item has-dropdown is-hoverable">
-                            <span className="navbar-link">
-                                {item.text}
-                            </span>
-                            <div className="navbar-dropdown">
-                                {item.items.map((subItem, index) => subItem.text === '-' || !subItem.link ?
-                                (
-                                    <hr key={index} className="navbar-divider" />
-                                ):
-                                (
-                                    <Link key={subItem.text} className="navbar-item" to={subItem.link}>
-                                        {subItem.text}
-                                    </Link>
-                                ))}
-                            </div>
-                        </div>
-                    ) :
-                    (
-                        <Link key={item.text} to={item.link} className="navbar-item">
-                            {item.text}
-                        </Link>
-                    )
-                )}
+                <button 
+                    className={classNames("navbar-burger", { "is-active": isMenuOpen })} 
+                    aria-label="menu" 
+                    aria-expanded="false" 
+                    data-target="navbarBasicExample"
+                    onClick={() => setMenuOpen(!isMenuOpen)}
+                >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </button>
             </div>
 
-            <div className="navbar-end">
-                <div className="navbar-item">
-                    <div className="buttons">
-                        <a className="button is-primary" href="/signup">
-                            <strong>Sign up</strong>
-                        </a>
-                        <a className="button is-light" href="/signin">
-                            Log in
-                        </a>
+            <div id="navbarBasicExample" className={classNames("navbar-menu", { "is-active": isMenuOpen })}>
+                <div className="navbar-start">
+                    {menu.map(item => item.items ? 
+                        (
+                            <div key={item.text} className="navbar-item has-dropdown is-hoverable">
+                                <span className="navbar-link">
+                                    {item.text}
+                                </span>
+                                <div className="navbar-dropdown">
+                                    {item.items.map((subItem, index) => subItem.text === '-' || !subItem.link ?
+                                    (
+                                        <hr key={index} className="navbar-divider" />
+                                    ):
+                                    (
+                                        <Link key={subItem.text} className="navbar-item" to={subItem.link}>
+                                            {subItem.text}
+                                        </Link>
+                                    ))}
+                                </div>
+                            </div>
+                        ) :
+                        (
+                            <Link key={item.text} to={item.link} className="navbar-item">
+                                {item.text}
+                            </Link>
+                        )
+                    )}
+                </div>
+
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <a className="button is-primary" href="/signup">
+                                <strong>Sign up</strong>
+                            </a>
+                            <a className="button is-light" href="/signin">
+                                Log in
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </nav>
-)
+        </nav>
+    );
+}
 
 export default TopMenu;
