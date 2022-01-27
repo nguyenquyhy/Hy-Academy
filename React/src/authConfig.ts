a4d532bef04cd2c2e1278b2ec13102995cf41071
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2022-01-27 00:33:07 -0500

    Add optional redirectUri and state for login

--- React/src/authConfig.ts
+++ React/src/authConfig.ts
@@ -1,21 +1,25 @@
-if (!process.env.REACT_APP_B2C_CLIENT_ID) throw new Error("CLIENT_ID must have value");
-if (!process.env.REACT_APP_B2C_API_SCOPE) throw new Error("API_SCOPE must have value");
-if (!process.env.REACT_APP_B2C_AUTHORITY) throw new Error("AUTHORITY must have value");
-if (!process.env.REACT_APP_B2C_SIGNUP_SIGNIN) throw new Error("SIGNUP_SIGNIN must have value");
-if (!process.env.REACT_APP_B2C_EDIT_PROFILE) throw new Error("EDIT_PROFILE must have value");
-if (!process.env.REACT_APP_B2C_RESET_PASSWORD) throw new Error("RESET_PASSWORD must have value");
+import { RedirectRequest } from "@azure/msal-browser";
+
+if (!process.env.REACT_APP_B2C_CLIENT_ID) throw new Error("CLIENT_ID must have value");
+if (!process.env.REACT_APP_B2C_API_SCOPE) throw new Error("API_SCOPE must have value");
+if (!process.env.REACT_APP_B2C_AUTHORITY) throw new Error("AUTHORITY must have value");
+if (!process.env.REACT_APP_B2C_SIGNUP_SIGNIN) throw new Error("SIGNUP_SIGNIN must have value");
+if (!process.env.REACT_APP_B2C_EDIT_PROFILE) throw new Error("EDIT_PROFILE must have value");
+if (!process.env.REACT_APP_B2C_RESET_PASSWORD) throw new Error("RESET_PASSWORD must have value");
 
 
 export const msalConfig = {        
     auth: {
         clientId: process.env.REACT_APP_B2C_CLIENT_ID ,
         authority: process.env.REACT_APP_B2C_SIGNUP_SIGNIN,
-        knownAuthorities: [process.env.REACT_APP_B2C_AUTHORITY]       
+        knownAuthorities: [process.env.REACT_APP_B2C_AUTHORITY]
     }
 };
 
-export const loginRequest = {
-    scopes: [process.env.REACT_APP_B2C_API_SCOPE]
+export const loginRequest: RedirectRequest = {
+    scopes: [process.env.REACT_APP_B2C_API_SCOPE],
+    redirectUri: process.env.REACT_APP_B2C_REDIRECT_URL,
+    state: process.env.REACT_APP_B2C_STATE
 };
 
 export const editProfile = {
