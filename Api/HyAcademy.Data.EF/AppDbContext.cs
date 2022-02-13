da3c21642e7913a06ef8a4826b739ee34ada187f
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2022-02-13 17:44:50 -0500

    Rename all table name to use class name directly

--- Api/HyAcademy.Data.EF/AppDbContext.cs
+++ Api/HyAcademy.Data.EF/AppDbContext.cs
@@ -11,4 +11,11 @@ public class AppDbContext : DbContext
 
     public DbSet<Profile> Profiles { get; set; } = null!;
     public DbSet<Course> Courses { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Override table name for DbSet to ensure all table name use class name directly
+        modelBuilder.Entity<Profile>().ToTable(nameof(Profile));
+        modelBuilder.Entity<Course>().ToTable(nameof(Course));
+    }
 }
