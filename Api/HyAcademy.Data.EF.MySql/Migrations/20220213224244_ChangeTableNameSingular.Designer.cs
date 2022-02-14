7f1f7fc464fe32950054494631405673203ab19a
Nguyễn Quý Hy <nguyenquyhy@live.com.sg>
2022-02-13 19:06:50 -0500

    Rename all table name to use class name directly (#83)

--- Api/HyAcademy.Data.EF.MySql/Migrations/20220213224244_ChangeTableNameSingular.Designer.cs
+++ Api/HyAcademy.Data.EF.MySql/Migrations/20220213224244_ChangeTableNameSingular.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HyAcademy.Data.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HyAcademy.Data.EF.MySql.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220213224244_ChangeTableNameSingular")]
+    partial class ChangeTableNameSingular
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Api/HyAcademy.Data.EF.MySql/Migrations/20220213224244_ChangeTableNameSingular.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HyAcademy.Data.EF.MySql.Migrations
+{
+    public partial class ChangeTableNameSingular : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoleAssignment_Courses_CourseId",
+                table: "RoleAssignment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoleAssignment_Profiles_ProfileId",
+                table: "RoleAssignment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Profiles",
+                table: "Profiles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Courses",
+                table: "Courses");
+
+            migrationBuilder.RenameTable(
+                name: "Profiles",
+                newName: "Profile");
+
+            migrationBuilder.RenameTable(
+                name: "Courses",
+                newName: "Course");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Profile",
+                table: "Profile",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Course",
+                table: "Course",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoleAssignment_Course_CourseId",
+                table: "RoleAssignment",
+                column: "CourseId",
+                principalTable: "Course",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoleAssignment_Profile_ProfileId",
+                table: "RoleAssignment",
+                column: "ProfileId",
+                principalTable: "Profile",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoleAssignment_Course_CourseId",
+                table: "RoleAssignment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoleAssignment_Profile_ProfileId",
+                table: "RoleAssignment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Profile",
+                table: "Profile");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Course",
+                table: "Course");
+
+            migrationBuilder.RenameTable(
+                name: "Profile",
+                newName: "Profiles");
+
+            migrationBuilder.RenameTable(
+                name: "Course",
+                newName: "Courses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Profiles",
+                table: "Profiles",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Courses",
+                table: "Courses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoleAssignment_Courses_CourseId",
+                table: "RoleAssignment",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoleAssignment_Profiles_ProfileId",
+                table: "RoleAssignment",
+                column: "ProfileId",
+                principalTable: "Profiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
